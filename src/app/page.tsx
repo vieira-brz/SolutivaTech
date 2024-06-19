2b6d05d6a2309b78c09d798dcf3949a4b21f4660
vieira-brz <vieirabraz2003@gmail.com>
2024-06-19 11:14:45 -0300

    fix: Bug imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import Footer from '../components/Footer';
-import About from '@/components/About';
+import About from '../components/About';
 
 const Home = () => {
   return (
